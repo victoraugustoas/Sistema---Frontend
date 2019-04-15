56f61e3af11f9ca435b1acd6c7f58917dd34a740
Victor Augusto Andrade Silva <vaas@icomp.ufam.edu.br>
2019-04-15 11:08:09 -0400

    Melhorias de visualização na página inicial
    > Adicionado o font awesome para ícone no menu
    
    > Adicionado css para o componente Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
+import './Navbar.css'
+import { Link } from 'react-router-dom'
 import logo from './logo_bf2.png'
 
 export default props => (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg py-3">
 
-        {/* <a className="navbar-brand" href="/">Navbar</a> */}
-        <img src={logo} alt="" />
+        <div className="logo">
+            <Link to='/'>
+                <img src={logo} alt={"Logo"} />
+            </Link>
+        </div>
 
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+            <i class="fas fa-ellipsis-v icon-menu"></i>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+            <ul className="navbar-nav ml-auto">
+                <li className="ml-3 mt-3 nav-item">
+                    <a className="btn button" href="/">Home</a>
                 </li>
 
-                <li className="nav-item">
-                    <a className="nav-link" href="/">Link</a>
-                </li>
-
-                <li className="dropdown">
-                    <a className="btn btn-secondary dropdown-toggle" href="/"
+                <li className="ml-3 mt-3 dropdown">
+                    <a className="btn button dropdown-toggle" href="/"
                         role="button" id="dropdownMenuLink" data-toggle="dropdown"
                         aria-haspopup="true" aria-expanded="false">
                         Categorias
@@ -33,7 +34,10 @@ export default props => (
                             return <a className="dropdown-item" key={idx.toString()} href={category.path}>{category.title}</a>
                         })}
                     </div>
+                </li>
 
+                <li className="ml-3 mt-3 nav-item">
+                    <a href="/posts" className="btn button">Posts</a>
                 </li>
 
             </ul>
